adf5aa758fc421f56e16174a760738d1e4cf2925
sssamooo <155848229+sssamooo@users.noreply.github.com>
2025-09-29 21:47:13 +0200

    Grid
    items s databazy do gridu

--- seeds/1757403684083_new_seed.ts
+++ seeds/1757403684083_new_seed.ts
@@ -3,21 +3,70 @@ import type { Kysely } from 'kysely';
 import { faker } from '@faker-js/faker';
 
 export async function seed(db: Kysely<DB>): Promise<void> {
-	await db.deleteFrom('authors').execute();
+	await db.deleteFrom("songs").execute();
+  await db.deleteFrom("albums").execute();
+  await db.deleteFrom("authors").execute();
 
-	for (let i = 1; i <= 10; i++) {
-		await db.insertInto('authors').values({
-			name: faker.music.artist(),
-			bio: faker.lorem.sentence(),
-		}).execute();
-	}
+  for (let i = 0; i < 20; i += 1) {
+    const numBioParagraphs = faker.number.int({ min: 0, max: 5 });
 
-	for (let i = 1; i <= 10; i++) {
-		await db.insertInto('songs').values({
-			album_id: i,
-			name: faker.music.songName(),
-			duration: Math.floor(Math.random() * 180)
-		}).execute();
-	}
-	
+    const bio =
+      numBioParagraphs !== 0 ? faker.lorem.paragraph(numBioParagraphs) : null;
+
+    await db
+      .insertInto("authors")
+      .values({
+        name: faker.music.artist(),
+        bio,
+      })
+      .execute();
+  }
+
+  const authors = await db.selectFrom("authors").selectAll().execute();
+
+  for (const author of authors) {
+    const numAlbums = faker.number.int({ min: 0, max: 10 });
+		const numSongs = faker.number.int({ min:10, max:20 })
+
+    console.log(author.name, numAlbums, numSongs);
+
+    for (let i = 0; i < numAlbums; i += 1) {
+      await db
+        .insertInto("albums")
+        .values({
+          author_id: author.id,
+          name: faker.music.album(),
+          release_date: faker.date.past().getTime(),
+        })
+        .execute();}
+    }
+
+		const albums = await db.selectFrom("albums").selectAll().execute();
+
+		for (const album of albums) {
+			const typeOfAlbum = faker.number.int({ min: 0, max: 9 });
+			let numSongs = 1;
+
+			if(typeOfAlbum < 2) {
+				numSongs = 1;
+			} else if (typeOfAlbum < 5) {
+				numSongs = faker.number.int({ min: 4, max: 6 });
+			}else {
+				numSongs = faker.number.int({ min: 10, max: 20 });
+			}
+
+
+			console.log(album.name, numSongs);
+
+			for (let i = 0; i < numSongs; i += 1) {
+				await db
+					.insertInto("songs")
+					.values({
+						album_id: album.id,
+						name: faker.music.songName(),
+						duration: faker.number.int({ min: 60, max: 240 }),
+					})
+					.execute();
+			}
+		}
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,50 @@
+import { DB } from "@/lib/db-types";
+import SQLite from "better-sqlite3";
+import { Kysely, SqliteDialect } from "kysely";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const dialect = new SqliteDialect({ database: new SQLite("db.sqlite") });
+  const db = new Kysely<DB>({ dialect });
+
+  const albums = await db.selectFrom("albums").select(["albums.id", "albums.name", "albums.release_date"]).execute();
+
+  const result = await db
+  .selectFrom('albums')
+  .innerJoin('authors', 'albums.author_id', 'authors.id')
+  .select(['albums.id', 'albums.name', 'authors.name as author_name'])
+  .execute()
+
+  console.log(result)
+  console.log(albums[0])
+  
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
             aria-hidden
-            src="/spotify-logo.png"
+            src="/Spotify_Full_Logo_RGB_Green.png"
             alt="spotify icon"
-            width={120}
-            height={120}
-            className="animate-spin"
+            width={300}
+            height={300}
+            className="m-auto mb-25"
           />
+
+        
+
+        <div className="grid grid-cols-6 gap-12">
+
+          {albums.map((album) => (
+            <div key={album.id} className="bg-blue-500 p-8 rounded-md font-semibold font-mono">
+              {album.name}: {new Date(album.release_date).toDateString()}
+            </div>
+            ))}
+
+        </div>
+
+        
+          
+
           
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
