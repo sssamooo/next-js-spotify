64c072932aa5156bc3f180c38dc70e8be3f77670
sssamooo <155848229+sssamooo@users.noreply.github.com>
2026-01-19 19:18:28 +0100

    popup instead of a new page for editing and creating playlists

--- src/app/playlists/[id]/page.tsx
+++ src/app/playlists/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getDb } from "@/lib/db";
-import Link from 'next/link'
 import RemovePlaylistSongButton from "./RemovePlaylistSongButton";
 import RemovePlaylistButton from "./RemovePlaylistButton";
+import { UpdatePlaylistButton } from "./UpdatePlaylistButton";
 
 export default async function PlaylistDetail({
   params,
@@ -14,52 +14,50 @@ export default async function PlaylistDetail({
   const playlistId = parseInt(id);
 
   const result = await db
-  .selectFrom("playlists_songs")
-  .where("playlist_id", "=", playlistId)
-  .innerJoin("songs", "songs.id", "playlists_songs.song_id")
-  .select(["songs.id", "songs.name"])
-  .execute();
+    .selectFrom("playlists_songs")
+    .where("playlist_id", "=", playlistId)
+    .innerJoin("songs", "songs.id", "playlists_songs.song_id")
+    .select(["songs.id", "songs.name"])
+    .execute();
 
 
   const playlistName = await db
-  .selectFrom("playlists")
-  .where("playlists.id", "=", playlistId)
-  .selectAll()
-  .execute();
+    .selectFrom("playlists")
+    .where("playlists.id", "=", playlistId)
+    .selectAll()
+    .execute();
 
   const name = playlistName[0]?.name ?? "Unknown";
-  
-  
+
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Link className="btn" href={`/playlists/${playlistId}/edit`}>
-          Edit
-        </Link>
+        <UpdatePlaylistButton playlistId={playlistId} playlistName={name} />
 
         <h1 className="text-white text-3xl font-bold m-auto">{name}</h1>
-        
-        <table className="table">
-          
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
 
-            <tbody>
-              {result.map((result, i) => (
-                <tr key={result.id}>
-                  <td>{i + 1}</td>
-                  <td>{result.name}</td>
-                  <td>
-                    <RemovePlaylistSongButton playlistId={playlistId} songId={result.id} />
-                  </td>
-                </tr>
-               ))}
-            </tbody>   
+        <table className="table">
+
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {result.map((result, i) => (
+              <tr key={result.id}>
+                <td>{i + 1}</td>
+                <td>{result.name}</td>
+                <td>
+                  <RemovePlaylistSongButton playlistId={playlistId} songId={result.id} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
 
         </table>
 
--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,21 +1,20 @@
 import { getDb } from "@/lib/db";
 import Link from "next/link";
+import { CreatePlaylistButton } from "./[id]/CreatePlaylistButton";
 
 export default async function Home() {
   const db = getDb();
 
   const playlists = await db
-  .selectFrom("playlists")
-  .where("user_id", "=", 1)
-  .selectAll()
-  .execute();
+    .selectFrom("playlists")
+    .where("user_id", "=", 1)
+    .selectAll()
+    .execute();
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Link className="btn" href="/playlists/new">
-          Create Playlist
-        </Link>
+        <CreatePlaylistButton />
 
         <div className="grid grid-cols-4 gap-12">
 
@@ -25,7 +24,7 @@ export default async function Home() {
               <p className="text-xs text-gray-500">ID: {playlist.id}</p>
               <p className="text-xs text-gray-500">User ID: {playlist.user_id}</p>
             </div>
-          ))} 
+          ))}
 
         </div>
 
